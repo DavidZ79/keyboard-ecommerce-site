6f6664dc8626e64f25333c5ea8369bd042fcf22b
David Zhou <david.m8f.z@gmail.com>
2023-03-09 22:35:49 -0500

    Added filler keyboard images

--- keyboard-ecommerce-site/src/products.js
+++ keyboard-ecommerce-site/src/products.js
@@ -1,58 +1,57 @@
-import product1 from "./assets/products/1.png";
-import product2 from "./assets/products/2.png";
-import product3 from "./assets/products/3.png";
-import product4 from "./assets/products/4.png";
-import product5 from "./assets/products/5.png";
-import product6 from "./assets/products/6.webp";
-import product7 from "./assets/products/7.webp";
-import product8 from "./assets/products/8.webp";
-
+import product1 from "./assets/keyboards/keyboard 1.jpg";
+import product2 from "./assets/keyboards/keyboard 2.jpg";
+import product3 from "./assets/keyboards/keyboard 3.jpg";
+import product4 from "./assets/keyboards/keyboard 4.jpg";
+import product5 from "./assets/keyboards/keyboard 5.jpeg";
+import product6 from "./assets/keyboards/keyboard 6.jpeg";
+import product7 from "./assets/keyboards/keyboard 7.jpeg";
+import product8 from "./assets/keyboards/keyboard 8.jpeg";
 export const PRODUCTS = [
   {
     id: 1,
-    productName: "IPhone",
+    productName: "1",
     price: 999.0,
     productImage: product1,
   },
   {
     id: 2,
-    productName: "Macbook Pro 2022 (M1)",
+    productName: "2",
     price: 1999.0,
     productImage: product2,
   },
   {
     id: 3,
-    productName: "Cannon M50 Camera",
+    productName: "3",
     price: 699.0,
     productImage: product3,
   },
   {
     id: 4,
-    productName: "WLS Van Gogh Denim Jacket",
+    productName: "4",
     price: 228.0,
     productImage: product4,
   },
   {
     id: 5,
-    productName: "LED Light Strips",
+    productName: "5",
     price: 19.99,
     productImage: product5,
   },
   {
     id: 6,
-    productName: "SPECTRUM LS TEE",
+    productName: "6",
     price: 68.0,
     productImage: product6,
   },
   {
     id: 7,
-    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    productName: "7",
     price: 120.0,
     productImage: product7,
   },
   {
     id: 8,
-    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    productName: "8",
     price: 40.0,
     productImage: product8,
   },
